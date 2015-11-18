3e02b67af7dcd888e1ad2f8a1135618aba0d977f
Tom Honeybone <tomhon@hotmail.com>
2015-11-18 11:20:02 -0800

    1st Commit

---
+++ AppBackend/AppBackend/AuthenticationTestHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Threading;
+using System.Security.Principal;
+using System.Net;
+using System.Web;
+
+namespace AppBackend
+{
+    public class AuthenticationTestHandler
+    {
+    }
+}
