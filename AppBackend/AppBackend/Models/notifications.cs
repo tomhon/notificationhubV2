18ca70f4d6120d54c8ba5e666719fabe8df7b2ce
Tom Honeybone <tomhon@hotmail.com>
2015-11-18 16:05:03 -0800

    started to add to notifications class

---
+++ AppBackend/AppBackend/Models/notifications.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Azure.NotificationHubs;
+
+
+namespace AppBackend.Models
+{
+    public class Notifications
+    {
+        public static Notifications Instance = new Notifications();
+
+        public NotificationHubClient Hub { get; set; }
+
+        private Notifications()
+        {
+            Hub = NotificationHubClient.CreateClientFromConnectionString("Endpoint=sb://july23todolist.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=IoUiOyLVQtVTeTY2eam8Cg84P9Bj+pLJvQBzNBOq4HU=",
+                                                                         "tomhonnotification_hub");
+        }
+    }
+}
